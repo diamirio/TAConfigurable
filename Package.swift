a3e000f29de889b125994403aa8a44f048a031d1
Robin Mayerhofer <rm@tailored-apps.com>
2019-08-21 14:03:09 +0200

    [2] fix SwiftPM support

--- Package.swift
+++ Package.swift
@@ -2,5 +2,22 @@ import PackageDescription
 
 let package = Package(
     name: "TAConfigurable",
-    exclude: [ "TAConfigurableTests" ]
-)
+    products: [
+        .library(
+            name: "TAConfigurable",
+            targets: ["TAConfigurable"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "TAConfigurable",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "TAConfigurableTests",
+            dependencies: [
+                "TAConfigurable"
+            ]
+        )
+    ]
+)^
